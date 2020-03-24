057805bc129649157d0957c604473808814897f6
Krzysztof Krawczyk <44506319+kkrawczykpl@users.noreply.github.com>
2020-03-24 16:52:49 +0100

    Validation

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,16 +1,17 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import e = require('express');
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService){}
 
     @Get()
-    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
         if (Object.keys(filterDto).length) {
             return this.tasksService.getTasksWithFilter(filterDto);
         } else {
@@ -24,6 +25,7 @@ export class TasksController {
     }
 
     @Post()
+    @UsePipes(ValidationPipe)
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.tasksService.createTask(createTaskDto);
     }
@@ -36,8 +38,8 @@ export class TasksController {
     @Patch(':id/status')
     updateTaskById(
         @Param('id') id:string,
-        @Body('status') status: TaskStatus,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
     ): Task {
-        return this.tasksService.updateTaskById(id, status);
+        return this.tasksService.updateTaskStatus(id, status);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid/v4';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -45,14 +45,19 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find(task => task.id === id);
+        const found = this.tasks.find(task => task.id === id);
+        if (!found) {
+            throw new NotFoundException();
+        }
+        return found;
     }
 
     deleteTaskById(id: string): void {
-        this.tasks = this.tasks.filter(task => task.id !== id);
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter(task => task.id !== found.id);
     }
 
-    updateTaskById(id: string, status: TaskStatus): Task {
+    updateTaskStatus(id: string, status: TaskStatus): Task {
         const task = this.getTaskById(id);
         task.status = status;
         return task;
